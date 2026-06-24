iwata-1116/gascity-contrib

fix(dispatch): fall back to city control-dispatcher on rig decay
A rig-local control-dispatcher can sit asleep with reason runtime-missing
for weeks. Every graph.v2 molecule's auto-injected workflow-finalize step
is pinned to its session, so with the rig-local dispatcher absent the
molecule never reaches CLOSED and the stall is invisible until a halt
exposes the orphaned step beads.

ControlDispatcherBinding now falls back to the city-level dispatcher
(resolved with an empty rig context) when the rig-local one is detected
runtime-missing at sling time. Detection reads the city-scoped session
bead's lifecycle reason and is injected via
Deps.ControlDispatcherRuntimeMissing, keeping graphroute a pure decorator
(the rig-scoped routing store cannot see city session beads). The fallback
fires only when routing is rig-scoped, the rig dispatcher is
runtime-missing, and the city dispatcher is a distinct agent; otherwise the
original binding is kept.

Both control-route entry paths are covered: the graph.v2 workflow
decoration path and the dynamic-fragment expansion path
(decorateDynamicFragmentRecipe), which now resolves the control route with
the same dependency set so a check/fanout fragment cannot bypass the
health gate.

Each re-routed control step is stamped
gc.control_dispatcher_fallback=<rig-local>-><city> so operators can detect
a decayed rig-local dispatcher via
`bd list --has-metadata-key gc.control_dispatcher_fallback`, rather than
the fallback becoming a new silent decay.

Detection contract and observability documented in
engdocs/architecture/dispatch.md.

Closes #3454.

Co-Authored-By: Claude Opus 4.8 (1M context) <noreply@anthropic.com>

cmd/gc/cmd_convoy_dispatch.go
@@ -558,7 +558,7 @@ func decorateDynamicFragmentRecipe(fragment *formula.FragmentRecipe, source bead
 	if routingRigContext == "" {
 		routingRigContext = graphRouteRigContext(defaultRoute.QualifiedName)
 	}
-	controlRoute, err := controlDispatcherBinding(store, cityName, cfg, routingRigContext)
+	controlRoute, err := graphroute.ControlDispatcherBinding(store, cityName, cfg, routingRigContext, cliGraphrouteDeps(cityPath))
 	if err != nil {
 		return err
 	}

gastownhall/gascity

cmd/gc/cmd_convoy_dispatch.go
@@ -564,7 +564,7 @@ func decorateDynamicFragmentRecipe(fragment *formula.FragmentRecipe, source bead
 		if binding, ok := controlRoutes[rigContext]; ok {
 			return binding, nil
 		}
-		binding, err := controlDispatcherBinding(store, cityName, cfg, rigContext)
+		binding, err := graphroute.ControlDispatcherBinding(store, cityName, cfg, rigContext, cliGraphrouteDeps(cityPath))
 		if err != nil {
 			return graphRouteBinding{}, err
 		}