c58e55b8ffee26f28408fea5912b75ea7f5ccb96
Jacob Hausler <jacob.hausler@gmail.com>
2026-09-05 03:47:01 -0500

    test: recompute resourcecensus audit baseline after rebase (subprocess 657/188->660/189, fixed_sleep 477/172->481/173); TESTING.md regenerated via -update

--- internal/testpolicy/resourcecensus/census.go
+++ internal/testpolicy/resourcecensus/census.go
@@ -125,8 +125,8 @@ var bootstrapPolicy = Ledger{
 		{
 			Scope:           ScopeAll,
 			Resource:        ResourceSubprocess,
-			BaselineCalls:   663,
-			BaselineFiles:   192,
+			BaselineCalls:   666,
+			BaselineFiles:   193,
 			ReportedCalls:   495,
 			ReportedFiles:   135,
 			OwnerBead:       "ga-cp3hwi",
@@ -138,8 +138,8 @@ var bootstrapPolicy = Ledger{
 		{
 			Scope:           ScopeAll,
 			Resource:        ResourceFixedSleep,
-			BaselineCalls:   484,
-			BaselineFiles:   173,
+			BaselineCalls:   488,
+			BaselineFiles:   174,
 			ReportedCalls:   447,
 			ReportedFiles:   157,
 			OwnerBead:       "ga-cp3hwi",
